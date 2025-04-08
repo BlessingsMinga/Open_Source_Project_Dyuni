969670cba49b86ac12c2f5a38b6d911dafd1cca2
Elijah Mhango <khellomhango@gmail.com>
2025-04-08 21:29:32 +0200

    First commit

--- verba_reader_app/lib/screens/book_detail.dart
+++ verba_reader_app/lib/screens/book_detail.dart
@@ -107,20 +107,28 @@ class BookDetail extends StatelessWidget {
                       onPressed: () {
                         _launchURL(buyLink);
                       },
-                      child: const Text('More Info',style: TextStyle(fontSize: 10, letterSpacing: 1.0,fontWeight: FontWeight.bold, color: Colors.black), textAlign: TextAlign.center,),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green[700],
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
-                          padding: EdgeInsets.only(
-                              right: 20, left: 25, top: 15, bottom: 15))
-
-                  ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+    child: ElevatedButton(
+    onPressed: () {
+    // Handle More Info button press
+    },
+    style: ElevatedButton.styleFrom(
+    backgroundColor: Colors.green[700],
+    shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.circular(10),
+    ),
+    padding: const EdgeInsets.symmetric(
+    horizontal: 25,
+    vertical: 15,
+    ),
+    ),
+    child: const Text(
+    'More Info',
+    textAlign: TextAlign.center,
+    style: TextStyle(
+    fontSize: 10,
+    letterSpacing: 1.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    ),
+    ),
+    ),
