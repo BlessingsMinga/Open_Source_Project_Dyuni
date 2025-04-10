49aeddd8bbe9055b73960ce8f2288c275c63af4c
Raymond Pute <150113920+raymondPute@users.noreply.github.com>
2025-04-10 17:08:19 +0200

    Delete verba_reader_app directory

--- verba_reader_app/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'screens/home_screen.dart';
-import 'services/google_books_api.dart';
-
-void main() {
-  runApp(const MyApp());
-}
