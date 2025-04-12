190e7b6b38460e26420d0147bbd3a5b798b1ccef
Raymond Pute <raymondpute22@gmail.com>
2025-04-12 14:27:55 +0200

    material app

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+        ChangeNotifierProvider(create: (_) => GoogleBooksApi()),
+    ],
+
+    child: MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'EBook reader Application',
