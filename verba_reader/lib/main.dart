bfea0e81085a3f96476b2798e141bb31e76efdfd
Olivechimphepo <olivechimphepo8@gmail.com>
2025-04-14 18:02:31 +0200

    book_detail

---
+++ verba_reader/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'book_detail.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Book App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+      routes: {
+        '/bookDetail': (context) => BookDetailPage(),
+      },
+    );
+  }
+}
+
+BookDetailPage() {}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go to Book Details'),
+          onPressed: () {
+            Navigator.pushNamed(context, '/bookDetail');
+          },
+        ),
+      ),
+    );
+  }
+}
