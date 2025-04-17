da54cca7dc264c5c24cd3212c495dedc99f6107b
Raymond Pute <raymondpute22@gmail.com>
2025-04-17 19:33:24 +0200

    changed lines of code

--- verba_reader/lib/main.dart
+++ verba_reader/lib/main.dart
@@ -1,42 +1,5 @@
 import 'package:flutter/material.dart';
-import 'book_detail.dart';
+import 'package:provider/provider.dart';
+import 'screens/home_screen.dart';
+import 'services/google_books_api.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Book App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomePage(),
-      routes: {,
-      },
-    );
-  }
-}
-
-BookDetailPage() {}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go to Book Details'),
-          onPressed: () {
-            Navigator.pushNamed(context, '/bookDetail');
-          },
-        ),
-      ),
-    );
-  }
-}
